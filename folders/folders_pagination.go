103c51b689b81ddcca3e5d366e2b0fe7d0e2ad18
Sawako Goshima <sawako@192-168-1-112.tpgi.com.au>
2023-11-13 19:00:00 +1100

    Created pagination function at folders_pagination.go. And added test code about pagination function at folders_test.go.

--- folders/folders_pagination.go
+++ folders/folders_pagination.go
@@ -1,3 +1,107 @@
 package folders
 
 // Copy over the `GetFolders` and `FetchAllFoldersByOrgID` to get started
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+
+	"github.com/gofrs/uuid"
+)
+
+// Pagination parameters added to the request struct
+type PaginatedFetchFolderRequest struct {
+	OrgID  uuid.UUID
+	Cursor string // Cursor is the pagination token
+	Limit  int    // Limit is the number of items per page
+}
+
+// Pagination token and data added to the response struct
+type PaginatedFetchFolderResponse struct {
+	Folders    []*Folder
+	NextCursor string // NextCursor is the pagination token for the next page
+}
+
+// Pagination logic in GetAllFolders
+func GetPaginatedAllFolders(req *PaginatedFetchFolderRequest) (*PaginatedFetchFolderResponse, error) {
+	// Use GetAllFolders to fetch all folders
+	nonPaginatedResponse, err := GetAllFolders(&FetchFolderRequest{OrgID: req.OrgID})
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch all folders: %w", err)
+	}
+	allFolders := nonPaginatedResponse.Folders
+
+	// Parse the cursor to get the starting point for this page
+	startingAfter, err := parsePaginationToken(req.Cursor)
+	if err != nil {
+		return nil, err // appropriate error handling
+	}
+
+	// Find the starting index based on the cursor's last ID
+	startIndex := 0
+	if startingAfter != nil {
+		for i, folder := range allFolders {
+			if folder.Id == startingAfter.LastID {
+				startIndex = i + 1
+				break
+			}
+		}
+	}
+
+	// Calculate the endIndex, making sure we don't go beyond the number of available folders
+	endIndex := startIndex + req.Limit
+	if endIndex > len(allFolders) {
+		endIndex = len(allFolders)
+	}
+
+	// Create the paginated slice of folders
+	foldersPage := allFolders[startIndex:endIndex]
+
+	// Generate the next cursor token if there's more data
+	var nextCursor string
+	if endIndex < len(allFolders) {
+		nextCursor = generatePaginationToken(foldersPage[len(foldersPage)-1])
+	}
+
+	// Return the paginated response
+	return &PaginatedFetchFolderResponse{
+		Folders:    foldersPage,
+		NextCursor: nextCursor,
+	}, nil
+}
+
+// PaginationTokenStruct defines the structure of the pagination token.
+
+type PaginationTokenStruct struct {
+	LastID uuid.UUID `json:"lastId"`
+}
+
+// Helper function to generate a pagination token from the last folder in a page
+func generatePaginationToken(lastFolder *Folder) string {
+	tokenStruct := PaginationTokenStruct{
+		LastID: lastFolder.Id,
+	}
+	tokenBytes, _ := json.Marshal(tokenStruct) // Ignoring error for brevity, but handle it in production
+	return base64.URLEncoding.EncodeToString(tokenBytes)
+}
+
+// Helper function to parse a pagination token back into the last ID
+func parsePaginationToken(token string) (*PaginationTokenStruct, error) {
+	if token == "" {
+		return nil, nil // No cursor provided, start from the beginning
+	}
+
+	tokenBytes, err := base64.URLEncoding.DecodeString(token)
+	if err != nil {
+		return nil, err
+	}
+
+	var tokenStruct PaginationTokenStruct
+	err = json.Unmarshal(tokenBytes, &tokenStruct)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tokenStruct, nil
+}
--- folders/folders_test.go
+++ folders/folders_test.go
@@ -41,3 +41,55 @@ func Test_GetAllFolders(t *testing.T) {
 	})
 
 }
+
+func TestGetPaginatedAllFolders(t *testing.T) {
+	orgID, err := uuid.FromString("3b9a868b-8cd9-4b6b-ba23-fd1e08f3e9fa")
+	if err != nil {
+		t.Fatalf("Failed to parse orgID: %v", err)
+	}
+
+	t.Run("Successful Pagination Retrieval", func(t *testing.T) {
+		// Define a request with a specific limit
+		req := &folders.PaginatedFetchFolderRequest{
+			OrgID:  orgID,
+			Limit:  1,  // Setting the limit to 1 for this test
+			Cursor: "", // Start with an empty cursor to get the first page
+		}
+
+		// Call the paginated function
+		resp, err := folders.GetPaginatedAllFolders(req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		assert.Len(t, resp.Folders, 1) // We expect only one folder due to the limit
+
+		// Check the details of the first folder
+		firstFolder := resp.Folders[0]
+		assert.Equal(t, "71702b42-aee8-4c03-a05c-1a0cc5102a85", firstFolder.Id.String())
+		assert.Equal(t, "sawa-test-1", firstFolder.Name)
+		assert.Equal(t, orgID, firstFolder.OrgId)
+		assert.False(t, firstFolder.Deleted)
+
+		// Now, use the returned cursor to get the next page
+		nextReq := &folders.PaginatedFetchFolderRequest{
+			OrgID:  orgID,
+			Limit:  1,
+			Cursor: resp.NextCursor, // Use the cursor from the previous response
+		}
+
+		nextResp, err := folders.GetPaginatedAllFolders(nextReq)
+		assert.NoError(t, err)
+		assert.NotNil(t, nextResp)
+		assert.Len(t, nextResp.Folders, 1) // We expect the second folder on the next page
+
+		// Check the details of the second folder
+		secondFolder := nextResp.Folders[0]
+		assert.Equal(t, "71702b42-aee8-4c03-a05c-1a0cc5102a86", secondFolder.Id.String())
+		assert.Equal(t, "sawa-test-2", secondFolder.Name)
+		assert.Equal(t, orgID, secondFolder.OrgId)
+		assert.True(t, secondFolder.Deleted)
+
+		// Ensure there's no next cursor after the second folder, indicating the end of the data
+		assert.Empty(t, nextResp.NextCursor)
+	})
+
+}
